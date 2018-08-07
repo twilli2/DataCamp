382264a9be7cfce30ea430b6eedf9df2201a1783
Trevor Williams <twilli2@psu.ds.pdx.edu>
2018-08-07 11:49:09 -0700

    The commit message is ideally descriptive. Some people prefer a very verbose message, others prefer a more concise one.

---
+++ Code/bridge data day 1.R
@@ -0,0 +1,77 @@
+install.packages("tidyverse")
+library(nycflights13)
+library(tidyverse)
+mpg
+ggplot(data=mpg)+
+geom_point(mapping = aes(x = class, y = drv))
+library(readxl)
+Hawthorne<-Hawthorne_Tilikum_Steel_daily_bike_counts_073118_1_ <- read_excel("Data/Hawthorne Tilikum Steel daily bike counts 073118 (1).xlsx",
+sheet = "Hawthorne", col_types = c("date","numeric", "numeric", "numeric"),skip = 1)
+View(Hawthorne_Tilikum_Steel_daily_bike_counts_073118_1_)
+Tilikum<-Hawthorne_Tilikum_Steel_daily_bike_counts_073118_1_ <- read_excel("Data/Hawthorne Tilikum Steel daily bike counts 073118 (1).xlsx",
+        sheet = "Tilikum", col_types = c("date","numeric", "numeric", "numeric"), skip = 1)
+View(Hawthorne_Tilikum_Steel_daily_bike_counts_073118_1_)
+Steel<-Hawthorne_Tilikum_Steel_daily_bike_counts_073118_1_ <- read_excel("Data/Hawthorne Tilikum Steel daily bike counts 073118 (1).xlsx",
+        sheet = "Steel", col_types = c("date", "numeric", "numeric", "numeric", "numeric"), skip = 1)
+View(Hawthorne_Tilikum_Steel_daily_bike_counts_073118_1_)
+bridgedata<-Hawthorne_Tilikum_Steel_daily_bike_counts_073118_1_
+utils::View(Hawthorne)
+utils::View(Tilikum)
+utils::View(Steel)
+summary(Hawthorne)
+library(dplyr)
+tally(Hawthorne)
+Hawthorne$Name<-("Hawthorne")
+Steel$Name<-("Steel")
+Tilikum$Name<-("Tilikum")
+names(Hawthorne)<-names(Tilikum) #force names of one sheet to the other
+names(Steel)<-c("date","lower","westbound","eastbound","total","Name")
+bikecounts<-bind_rows(Hawthorne,Tilikum,Steel %>% select(-lower)) #put all the sheets into one
+bikecounts %>%
+  group_by(Name)%>%
+  summarize(avg_daily_counts=mean(total,Na.rm=TRUE)) #filters out by average and remove NAs
+library(lubridate) #
+bikecounts %>%
+  group_by(Name,month(date))%>%
+  summarize(avg_daily_counts=mean(total,Na.rm=TRUE))
+bikecounts %>%
+  group_by(Name,month(date))%>%
+  summarize(avg_daily_counts=mean(total,Na.rm=TRUE/distinct(month)))
+utils::View(bikecounts %>%
+              group_by(Name,ym=floor_date(date,"month"))%>%
+              summarize(total_monthly_counts=sum(total),counts=n())%>%
+              group_by(Name,month(ym))%>%
+              summarize(avg_montly_count=mean(total_monthly_counts)))#control shift m for pipe
+              
+input_file <- "data/Hawthorne Tilikum Steel daily bike counts 073118_1.xlsx"
+bridge_name <- "Hawthorne"
+
+library(readxl)
+library(lubridate)
+# define a funtion that load bike counts data
+load_data <- function(input_file, bridge_name) {
+  bikecounts <- read_excel(input_file,
+                           sheet = bridge_name,
+                           skip = 1)
+  bikecounts$name <- bridge_name
+  bikecounts
+}
+
+Tilikum <- load_data(input_file, "Tilikum")
+Hawthorne <- load_data(input_file, "Hawthorne")
+
+# use the column names of Tilikum for Hawthorne
+names(Hawthorne) <- names(Tilikum)
+
+Steel <- load_data(input_file, "Steel")
+names(Steel) <- c("date", "lower", "westbound", "eastbound", "total", "name")
+
+# combine all three data frame for all three bridges
+bikecounts <- bind_rows(Hawthorne, 
+                        Tilikum, 
+                        Steel %>% select(-lower)) # exclude the `lower` col in Steel data frame
+
+# average daily bike counts by bridge
+bikecounts %>% 
+  group_by(Name) %>% 
+  summarize(avg_daily_counts=mean(total, na.rm=TRUE))
---
+++ Code/gis census day 2.R
@@ -0,0 +1,34 @@
+install.packages("tidycensus")
+library(tidycensus)
+library(tidyverse)
+library(dplyr)
+library(plyr)
+census_api_key("e6edb0be0f3892def6b04a8e181ee49fef629bc4", install=TRUE)
+m90 <- get_decennial(geography = "state", variables = "H043A001", year = 1990)
+
+utils::View(head(m90))
+
+m90 %>%
+  ggplot(aes(x = value, y = reorder(NAME, value))) + 
+  geom_point()
+v15 <- load_variables(2016, "acs5", cache = TRUE)
+
+utils::View(v15)
+
+o1<-get_acs(geography ="state", variables = "B00001_001E", year = 2016)
+o1 <- get_acs(geography = "tract", 
+                                   year = 2016, # 2012-2016
+                                   variables = "B00001_001E",  # Est Total
+                                   state = "OR", 
+                                   county = "Lane County",
+                                   geometry = TRUE) # load geometry/gis info
+ggplot(o1) + 
+  geom_sf(aes(fill = estimate)) +
+  coord_sf(datum = NA) + theme_minimal()
+## Install the mapview package if you haven't yet
+install.packages("mapview")
+library(sf)
+library(mapview)
+
+mapview(o1 %>% select(estimate), 
+        col.regions = sf.colors(10), alpha = 0.1)
